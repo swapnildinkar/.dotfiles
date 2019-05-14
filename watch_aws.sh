8d6f1d5e5d3b59a57d17f20846e343db7babe81c
Swapnil Dinkar <swapnildinkar@gmail.com>
2019-05-14 11:18:59 -0700

    watch_aws: kill existing session if it exists
    Signed-off-by: Swapnil Dinkar <swapnildinkar@gmail.com>

--- watch_aws.sh
+++ watch_aws.sh
@@ -3,11 +3,19 @@
 REFRESH_RATE=20
 REGION1=us-west-2
 REGION2=us-east-1
+SESSION_NAME=watch-aws
 
-tmux new-session -d -s foo "watch -n $REFRESH_RATE awless list instances -e -r $REGION1"
+tmux has-session -t $SESSION_NAME
+if [ $? -eq 0 ]
+then
+	echo "killing existing session"
+	tmux kill-session -t $SESSION_NAME
+fi
+
+tmux new-session -d -s $SESSION_NAME "watch -n $REFRESH_RATE awless list instances -e -r $REGION1"
 
 tmux split-window -dh -t 0 "watch -n $REFRESH_RATE awless list instances -r $REGION2"
 tmux split-window -v -t 0 "watch -n $REFRESH_RATE awless list volumes -r $REGION1"
 tmux split-window -v -t 2 "watch -n $REFRESH_RATE awless list volumes -r $REGION2"
 
-tmux -2 attach-session -t foo
+tmux -2 attach-session -t $SESSION_NAME
